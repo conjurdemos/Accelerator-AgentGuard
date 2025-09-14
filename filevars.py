a64e7cd8cb30a4fb0312471bf5ff2ee08a2f7416
Jody Hunt <Jody.Hunt@NG-JODY-H.local>
2025-09-14 10:47:28 -0500

    more verbose demo output

--- filevars.py
+++ filevars.py
@@ -8,15 +8,19 @@ from agent_guard_core.credentials import FileSecretsProvider, EnvironmentVariabl
 
 @EnvironmentVariablesManager.set_env_vars(FileSecretsProvider())
 async def myFunc() -> None:
+  print("\nin myFunc() - checking environment variables...")
   username = os.getenv("USERNAME")
   password = os.getenv("PASSWORD")
   print(f"Username: {username}, Password: {password}")
 
 if __name__ == '__main__':
+  print("\n\nRunning Agent Guard demo:\n")
+  print("\nin main() - checking environment variables...")
   if not os.getenv("USERNAME"):
     print("Username not found")
 
   asyncio.run(myFunc())
 
+  print("\nin main() - checking environment variables...")
   if not os.getenv("PASSWORD"):
     print("Password not found")
