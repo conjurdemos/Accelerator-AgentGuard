27c74dc119580e81448b4aa675a1c7c3dc4f145d
Jody Hunt <Jody.Hunt@NG-JODY-H.local>
2025-09-18 12:13:05 -0500

    added license & other files for public repo compliance.

---
+++ CONTRIBUTING.md
@@ -0,0 +1,3 @@
+# Contributing
+
+This is a set of demos managed by CyberArk. No contributions are currently being solicited or accepted.
--- filevars.py
+++ filevars.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
 
-import os, asyncio
+import os
+import asyncio
 
-#----------------------------------------
+# ----------------------------------------
 # Get secrets from File as env vars
-from agent_guard_core.credentials import FileSecretsProvider, EnvironmentVariablesManager
+from agent_guard_core.credentials import (
+    FileSecretsProvider,
+    EnvironmentVariablesManager,
+)
 
 @EnvironmentVariablesManager.set_env_vars(FileSecretsProvider())
 async def myFunc() -> None:
-  print("\nin myFunc() - checking environment variables...")
-  username = os.getenv("USERNAME")
-  password = os.getenv("PASSWORD")
-  print(f"Username: {username}, Password: {password}")
+    print("\nin myFunc() - checking environment variables...")
+    username = os.getenv("USERNAME")
+    password = os.getenv("PASSWORD")
+    print(f"Username: {username}, Password: {password}")
 
-if __name__ == '__main__':
-  print("\n\nRunning Agent Guard demo:\n")
-  print("\nin main() - checking environment variables...")
-  if not os.getenv("USERNAME"):
-    print("Username not found")
+if __name__ == "__main__":
+    print("\n\nRunning Agent Guard demo:\n")
+    print("\nin __main__: checking environment variables...")
+    if not os.getenv("USERNAME"):
+        print("Username not found")
 
-  asyncio.run(myFunc())
+    asyncio.run(myFunc())
 
-  print("\nin main() - checking environment variables...")
-  if not os.getenv("PASSWORD"):
-    print("Password not found")
+    print("\nin __main__:checking environment variables...")
+    if not os.getenv("PASSWORD"):
+        print("Password not found")
